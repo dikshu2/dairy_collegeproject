7a46b144261682df829c8360804334f0e107ec48
shobik-maharjan <shobitmah32@gmail.com>
2022-03-19 17:06:57 +0545

    try

--- signup.php
+++ signup.php
@@ -107,9 +107,8 @@
                 </div>
 
                 <p class="text-center text-muted mt-5 mb-0">Have already an account? <a href="#!" class="fw-bold text-body"><u>Login here</u></a></p>
-
               </form>
-              
+
             </div>
           </div>
         </div>
