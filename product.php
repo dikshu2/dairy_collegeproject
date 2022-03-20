9016f454abd9e96998f58a49dcadcc56e5b7fdaa
shobik-maharjan <shobitmah32@gmail.com>
2022-03-20 17:01:13 +0545

    small change

--- product.php
+++ product.php
@@ -23,14 +23,14 @@
 <div class="bc">
     <nav class="navbar navbar-expand-lg ">
   <div class="container">
-    <a class="navbar-brand" href="#">Vege</a>
+    <a class="navbar-brand" href="index.php">Vege</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarSupportedContent">
       <ul class="navbar-nav m-auto mb-2 mb-lg-0">
         <li class="nav-item">
-          <a class="nav-link active"  href="#">Home</a>
+          <a class="nav-link active"  href="index.php">Home</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#">product</a>
